36c71c344b80648bbc4f19882247b6777abaf381
Alper Taday <trshield@gmail.com>
2014-06-18 11:53:02 +0300

    Update receivedata.py

--- TestServer/receivedata.py
+++ TestServer/receivedata.py
@@ -1,46 +1,57 @@
-import socket, sys
-from struct import *
+import socket, sys, struct
+
+# the above code breaks down the packet into IP Header + TCP Header 
 
 def receiveData():
 
+	# create an INET, raw socket
 	s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 
+	# receive a packet
 	packet = s.recvfrom(65536)
-
+	
+	#packet string from tuple
 	packet = packet[0]
-
+	
+	# take first 20 characters for the ip header
 	ip_header = packet[0:20]
+	# header format: http://nmap.org/book/tcpip-ref.html
+	
+	# unpack them
+	iph = struct.unpack('!2B3H2BH4s4s' , ip_header)   
+	# the unpack function is used to break down the packet.
+	# format characters: https://docs.python.org/2/library/struct.html#format-characters
+	
+	# parse IP header
+	total_length = iph[2]
+	protocol_number = iph[6]
 
-	iph = unpack('!2B3H2BH4s4s' , ip_header)   
+	source_address = socket.inet_ntoa(iph[8]);
+	destination_address = socket.inet_ntoa(iph[9]);   
 
-	t_length = iph[2]
-	protocol = iph[6]
+	if protocol_number == 1:
+		protocol_id = 'ICMP'
+	elif protocol_number == 6:
+		protocol_id = 'TCP'
+	elif protocol_number == 17:
+		protocol_id = 'UDP'
 
-	s_addr = socket.inet_ntoa(iph[8]);
-	d_addr = socket.inet_ntoa(iph[9]);
-
-	protocol_s = protocol   
-
-	if protocol == 1:
-		protocol_s = 'ICMP'
-	if protocol == 6:
-		protocol_s = 'TCP'
-	if protocol == 17:
-		protocol_s = 'UDP'
-
+	
+	# take first 20 characters for the tcp header
 	tcp_header = packet[20:40]
+	
+	# unpack them
+	tcph = struct.unpack('!HHLLBBHHH' , tcp_header)
 
-	tcph = unpack('!HHLLBBHHH' , tcp_header)
-
-	dest_port = tcph[1]       
+	# parse TCP header
+	destination_port = tcph[1]       
 
 	data = { 
-		'proto' : protocol_s,
-		'source' : str(s_addr),
-		'dest' : str(d_addr),
-		'd_port' : str(dest_port),
-		'size' : str(t_length)
+		'protocol' : protocol_id,
+		'source address' : source_address,
+		'destination address' : destination_address,
+		'destination port' : destination_port,
+		'packet size' : total_length
 	}
 
 	return data
-
