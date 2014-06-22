a37d7c6975770dc036ca05427b8cba71f7434948
Aziz Bekkine <abekkine@gmail.com>
2014-06-22 11:45:15 +0300

    Bad Request konusu
    Bad Request mesajını, POST için yazdığınız metodu view için kullanmaya çalıştığınızda alıyorsunuz. saveData() fonksiyonunu hem GET, hem de POST metodlarını işleyebilecek şekilde değiştirdim. Eski index() fonksiyonunu kaldırıp, saveData() yı index olacak şekilde yeniden düzenledim. Kendi bilgisayarımda denediğimde çalıştığını gördüm. Siz de bir inceleyin, sorularınız olursa konuşuruz.

--- TestServer/app.py
+++ TestServer/app.py
@@ -1,28 +1,37 @@
 #!/usr/bin/env python
 from flask import Flask, request
-from receivedata import receiveData
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-
-def saveData():
-
-	#data = request.get_json(force=True) | BadRequest hatasini almamak icin bu kismi asagidaki gibi yazdim
-	data = receiveData()	
-	logfile = open("log.txt", "a")
-	logfile.write(str(data))
-	logfile.close()
-	
-	return index()
-	
 def index():
-	
-	r_file = open("log.txt", "r")
-	html = ""
-	for e in r_file:
-		html = "%s <p>%s</p>" % (html, e[:]) ##Postlama islemi veri kaybetmeden calisiyor fakat html formati olarak duzenleme kisminda tam olarak istedigimi elde edemedim.
-		return "%s" % html 
+
+	if request.method == 'POST':
+		# Save Data : This code is for POST only, not for viewing from browser.
+
+		# Get posted network activity from request body.
+		data = request.get_json(force=True)
+		# Open server-side log file and append received activity.
+		logfile = open("log.txt", "a")
+		logfile.write(str(data))
+		logfile.close()
+		# Inform posting client that everything is fine.
+		response = "ok"
+
+	elif request.method == 'GET':
+		# View Data : This code is for viewing accumulated data from browser.
+
+		# Open server-side log file.
+		r_file = open("log.txt", "r")
+		# Generate a simple html view from its contents.
+		html = ""
+		for e in r_file:
+			html = "%s <p>%s</p>" % (html, e)
+
+		# Return it for viewing inside browser.
+		response = html
+
+	return response
 
 if __name__ == '__main__':
 	app.run(debug=True)
