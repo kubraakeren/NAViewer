b2845acb5a2f1f811ea42d393e96689272d0d8ce
Kübra Akeren <kubraakeren@hotmail.com>
2014-07-11 15:17:22 +0300

    Update receivedata.py

--- TestServer/receivedata.py
+++ TestServer/receivedata.py
@@ -1,46 +1,46 @@
-import socket, sys
-from struct import *
+import socket, sys, struct
+from time import time
 
 def receiveData():
 
-	s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+        s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 
-	packet = s.recvfrom(65536)
+        packet = s.recvfrom(65536)
 
-	packet = packet[0]
+        packet = packet[0]
 
-	ip_header = packet[0:20]
+        ip_header = packet[0:20]
 
-	iph = unpack('!2B3H2BH4s4s' , ip_header)   
+        iph = struct.unpack('!2B3H2BH4s4s' , ip_header)
 
-	t_length = iph[2]
-	protocol = iph[6]
+        total_length = iph[2]
+        protocol_number = iph[6]
 
-	s_addr = socket.inet_ntoa(iph[8]);
-	d_addr = socket.inet_ntoa(iph[9]);
+        source_address = socket.inet_ntoa(iph[8]);
+        destination_address = socket.inet_ntoa(iph[9]);
 
-	protocol_s = protocol   
+        if protocol_number == 1:
+                protocol_id = 'ICMP'
+        if protocol_number == 6:
+                protocol_id = 'TCP'
+        if protocol_number == 17:
+                protocol_id = 'UDP'
 
-	if protocol == 1:
-		protocol_s = 'ICMP'
-	if protocol == 6:
-		protocol_s = 'TCP'
-	if protocol == 17:
-		protocol_s = 'UDP'
+        tcp_header = packet[20:40]
 
-	tcp_header = packet[20:40]
+        tcph = struct.unpack('!HHLLBBHHH' , tcp_header)
 
-	tcph = unpack('!HHLLBBHHH' , tcp_header)
+        destination_port = tcph[1]
 
-	dest_port = tcph[1]       
+        current_time = time()
 
-	data = { 
-		'proto' : protocol_s,
-		'source' : str(s_addr),
-		'dest' : str(d_addr),
-		'd_port' : str(dest_port),
-		'size' : str(t_length)
-	}
+        data =  {
+                'time' : current_time,
+                'proto' : protocol_id,
+                'source' : source_address,
+                'dest' : destination_address,
+                'd_port' : destination_port,
+                'size' : total_length
+                }
 
-	return data
-
+        return data
